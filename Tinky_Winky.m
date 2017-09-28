103f666781c84dda17bc7f3cc5005fb3923f8a97
Rubenia Borge <rubeniaolaska@gmail.com>
2017-09-27 22:25:56 -0600

    Add files via upload

---
+++ Tinky_Winky.m
@@ -0,0 +1,56 @@
+# Fly me to the moon...
+
+5 + 6
+3 - 2
+5 * 8
+1/2
+2^6
+1==2
+1~=2
+1&&0
+1||0
+xor(1,0)
+a = 3
+b = 'hi'
+c = (3 >= 1)
+a = pi;
+disp (a)
+disp(sprintf('2 decimals: %0.2f', a))
+disp(sprintf('6 decimals: %0.6f', a))
+
+format long
+a
+
+format short
+a
+
+A = [1 2; 3 4; 5 6]
+
+A = [1 2;
+3 4;
+5 6]
+
+v = [1 2 3]
+w = [1; 2; 3]
+
+z = 1:0.1:2
+
+a = ones(2,3)
+b = 2*ones(2,3)
+c = [2 2 2; 2 2 2]
+
+w = ones(1,3)
+w = zeros(1,3)
+w = rand(3,3)
+w = randn(1,3)
+w = -6 + sqrt(10)*(randn(1,10000));
+
+hist(w)
+hist(w,50)
+
+I = eye(4)
+
+size(sz = size(A))
+
+size(A,1)
+size(A,2)
